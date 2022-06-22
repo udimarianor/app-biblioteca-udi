06eeb6e2afaf9605a7983588af6d33d0caf77c4d
udi.mariano.r <udi.mariano.r@gmail.com>
2022-06-22 19:37:46 -0300

    despliegue local

--- target/generated-sources/annotations/com/spring/ejercicio01/entidades/Autor_.java
+++ target/generated-sources/annotations/com/spring/ejercicio01/entidades/Autor_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-16T16:21:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-22T19:34:36")
 @StaticMetamodel(Autor.class)
 public class Autor_ { 
 
--- target/generated-sources/annotations/com/spring/ejercicio01/entidades/Usuario_.java
+++ target/generated-sources/annotations/com/spring/ejercicio01/entidades/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-16T16:21:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-22T19:34:36")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
