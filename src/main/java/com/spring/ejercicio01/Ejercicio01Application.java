ff157a532ebce636751394d9bb349fba4353e111
udi.mariano.r <udi.mariano.r@gmail.com>
2022-06-23 16:34:18 -0300

    Update Ejercicio01Application.java

--- src/main/java/com/spring/ejercicio01/Ejercicio01Application.java
+++ src/main/java/com/spring/ejercicio01/Ejercicio01Application.java
@@ -2,16 +2,9 @@ package com.spring.ejercicio01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ejercicio01Application extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Ejercicio01Application.class);
-	}
+public class Ejercicio01Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ejercicio01Application.class, args);
